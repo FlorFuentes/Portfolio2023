67441e3aecdb1bdf7348454641322a052c405a8f
FlorFuentes <flopy_200412@hotmail.com>
2023-07-25 16:29:00 -0300

    @MediaQuery/foto de tarjeta

--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { DatosPortfolioService } from 'src/app/services/datos-portfolio.service';
 
 @Component({
@@ -10,7 +9,7 @@ import { DatosPortfolioService } from 'src/app/services/datos-portfolio.service'
 export class ProyectosComponent {
   proyec:any;
   proyecLista:any;
-  constructor( private router:Router,private datosProyec:DatosPortfolioService){}
+  constructor( private datosProyec:DatosPortfolioService){}
 
   ngOnInit(): void{
     this.datosProyec.obtenerDatos().subscribe(data =>{
